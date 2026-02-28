d8e1e87c3760e5a46fac7df19ac3d45f9b2a06a7
Dushesa <andrejmanusa2@gmail.com>
2026-02-28 16:36:16 +0100

    1

--- resources/views/partials/Sidebar.blade.php
+++ resources/views/partials/Sidebar.blade.php
@@ -2,17 +2,17 @@
     
 </div>
 
-<div class="recommendations">
+<ul class="recommendations">
     <h2>Other Packs</h2>
     @foreach($recommendations as $index => $rec)
     <a href="{{ url($rec->page_url) }}">
-        <div class="card">
+        <li class="card">
             <img src="/storage{{ $rec->page_url }}/{{ basename($rec->page_url) === $rec->franchise ? $rec->franchise . '/' : '' }}{{ strtok($rec->images, ', ') }}" width="304" height="171" 
                 alt="{{ !empty(explode(' | ', $rec->image_alts)[0]) ? explode(' | ', $rec->image_alts)[0] : ucfirst($rec->title) . ' from ' . ucwords(str_replace('-', ' ', $rec->franchise)) . ' Resource Pack Preview' }}"
                 @if($index > 2) loading="lazy" decoding="async" @endif >
             <h2>{{ $rec->title }}</h2>
             <p>{{ $rec->min_desc }}</p>
-        </div>
+        </li>
     </a>
     @endforeach
-</div>
+</ul>
